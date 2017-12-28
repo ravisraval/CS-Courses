2e7bec0ad630e51e518b33cf1c6acd0c1fb8f96f
Ravi <raviraval@gmail.com>
2017-12-27 20:46:18 -0800

    complete algos hw 1

--- Stanford_Algorithms/1_design_analysis/assignment_1.rb
+++ Stanford_Algorithms/1_design_analysis/assignment_1.rb
@@ -11,21 +11,22 @@
 # (We do not require you to submit your code, so feel free to use any programming language you want --- just type the final numeric answer in the following space.)
 #
 # [TIP: before submitting, first test the correctness of your program on some small test files or your own devising. Then post your best test cases to the discussion forums to help your fellow students!]
-
+require 'byebug'
 def get_inversions(arr)
-  return arr if arr.length <= 1
-
+  return [arr, 0] if arr.length <= 1
   pivot = arr.length / 2
 
   sorted_left = get_inversions(arr[0...pivot])
   sorted_right = get_inversions(arr[pivot..-1])
 
-  return merge(sorted_left, sorted_right)
+  merged = merge(sorted_left[0], sorted_right[0])
+
+  [merged[0], sorted_left[1] + sorted_right[1] + merged[1]]
 end
 
-def merge(left, right, inversions = 0)
-  p left
-  p right
+def merge(left, right)
+  inversions = 0
+
   result_arr = []
 
   left_idx = 0
@@ -33,12 +34,13 @@ def merge(left, right, inversions = 0)
 
   until left_idx == left.length || right_idx == right.length
     if left[left_idx] < right[right_idx]
+
       result_arr.push(left[left_idx])
       left_idx += 1
     else
       result_arr.push(right[right_idx])
       right_idx += 1
-      inversions += 1
+      inversions += left.length - left_idx
     end
   end
 
@@ -49,7 +51,10 @@ def merge(left, right, inversions = 0)
     inversions += left.length - left_idx
   end
 
-  inversions
+  [result_arr, inversions]
 end
 
-p get_inversions([1,2,3,4,5])
+new_arr = open('./IntegerArray.txt').each_line.map { |line| line.chomp.to_i }
+# arr = [1,6,3,2,4,5]
+# puts "#{arr} has #{get_inversions(arr)[1]} inversions"
+p get_inversions(new_arr)[1]
